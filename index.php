edbdb76af2d32b21f7882912a1441e0056b2e990
pushnoy100500 <romanenko.igor2014@outlook.com>
2016-03-05 22:20:51 -0500

    initial setup

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+ ?>
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+     <link rel="stylesheet" href="stylesheets/bootstrap.min.css">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+     <script type="text/javascript" src="js/bootstrap.min.js"></script>
+     <link rel="stylesheet" type="text/css" href="stylesheets/screen.css">
+   </head>
+   <body>
+    
+   </body>
+ </html>
